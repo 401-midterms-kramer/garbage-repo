a7844b4d96ec0e1fc73fe57e5276fbef421d32cb
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-08-21 18:54:08 -0700

    it almost there

--- promt-commands/__test__/prompt-command.test.js
+++ promt-commands/__test__/prompt-command.test.js
@@ -2,4 +2,9 @@
 
 let prompt = require ('../prompt-command.js');
 
-describe('Give a prompt for a username')
+describe('Give a prompts',() =>{
+    it('promts for a username',() =>{
+        let name = 'Liz' ;
+        expect(AWSusername).toBe('Liz');
+    })
+})
--- promt-commands/prompt-command.js
+++ promt-commands/prompt-command.js
@@ -8,10 +8,10 @@ prompt.start();
 prompt.get(schema, function(err,result){
     console.log('command-line input received:');
     console.log('AWSusername: '+ result.AWSusername);
-    console.log('github Repo: '+ result.githubRepo);
+    console.log('github Repo: '+ result.githubRepoQuestion);
     console.log('env File: '+ result.envFile)
-    if(result.envFile === 'yes'||'y'){
-        console.log('env.filePath ' + result.envFilePath)
+    if(result.envFile === 'no'||'n'){
+        console.log('You have selected no env file to upload');
     };
 });
 

09bfbe141916dc567e372fd15f012b14733dcbc3
lizkavalski <lizkavalski@DESKTOP-IM8BUGM.localdomain>
2019-08-26 21:28:36 -0700

    add doc to the prompt-inquirer.js

---
+++ promt-commands/__test__/prompt-inquirer.test.js
@@ -0,0 +1,10 @@
+'use strict'
+const inquirer = require ('inquirer');
+require ('../prompt-inquirer');
+
+describe('Give a prompts',() =>{
+    it('promts for a github URL',() =>{
+        expect().toBe();
+    })
+})
+

2cc4bf5e3e0bd893cc97f7d410d2e1a550384f3d
leeroywking <leeroywking@gmail.com>
2019-08-26 21:51:51 -0700

    got the dns thing working a bit

--- aws-commands/leednsattempt.js
+++ aws-commands/leednsattempt.js
@@ -1,18 +1,34 @@
 const AWS = require('aws-sdk');
-const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+AWS.config.update({ region: 'us-west-2' })
+const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 
 /* This example describes the specified instance. */
 
 var params = {
-    "image-id": [
-       "i-1234567890abcdef0"
-    ]
-   };
-   ec2.describeInstances(params, function(err, data) {
-     if (err) console.log(err, err.stack); // an error occurred
-     else     console.log(data);           // successful response
-     /*
-     data = {
-     }
-     */
-   });
+  Filters: [
+    {
+      Name: "image-id",
+      Values: [
+        "ami-05b7c1459926442c5"
+      ]
+    }
+  ]
+};
+let output = ''
+return ec2.describeInstances(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else data.Reservations.forEach(res => res.Instances.forEach(inst => {
+    if (inst.PublicDnsName) {
+      return inst.PublicDnsName
+    }
+    else {}
+  }));           // successful response
+  /*
+  data = {
+  }
+  */
+});
+//   ec2.waitFor('instanceStatusOk', params, function(err, data) {
+//     if (err) console.log(err, err.stack); // an error occurred
+//     else     console.log(data);           // successful response
+//   });
